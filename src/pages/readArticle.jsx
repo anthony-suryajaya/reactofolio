663f713de4e30448468fb87a6029b07602812646
Anthony Suryajaya <92085498+anthony-suryajaya@users.noreply.github.com>
2023-08-01 09:27:48 +0700

    Update readArticle.jsx
    Adding Google Analytics Tag

--- src/pages/readArticle.jsx
+++ src/pages/readArticle.jsx
@@ -31,6 +31,15 @@ const ReadArticle = () => {
 	return (
 		<React.Fragment>
 			<Helmet>
+				<!-- Google tag (gtag.js) -->
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-2641CR8ZSL"></script>
+				<script>
+				  window.dataLayer = window.dataLayer || [];
+				  function gtag(){dataLayer.push(arguments);}
+				  gtag('js', new Date());
+				
+				  gtag('config', 'G-2641CR8ZSL');
+				</script>
 				<title>{`${article().title} | ${INFO.main.title}`}</title>
 				<meta name="description" content={article().description} />
 				<meta name="keywords" content={article().keywords.join(", ")} />
