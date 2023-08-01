6f3410350f559d7f48f1467ebd757221423d40ab
Anthony Suryajaya <92085498+anthony-suryajaya@users.noreply.github.com>
2023-08-01 09:29:30 +0700

    Update contact.jsx
    Adding Google Analytics Tag

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -21,6 +21,15 @@ const Contact = () => {
 	return (
 		<React.Fragment>
 			<Helmet>
+				<!-- Google tag (gtag.js) -->
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-2641CR8ZSL"></script>
+				<script>
+				  window.dataLayer = window.dataLayer || [];
+				  function gtag(){dataLayer.push(arguments);}
+				  gtag('js', new Date());
+				
+				  gtag('config', 'G-2641CR8ZSL');
+				</script>
 				<title>{`Contact | ${INFO.main.title}`}</title>
 				<meta name="description" content={currentSEO.description} />
 				<meta
